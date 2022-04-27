c440e93dc29caf0373e007408584477c5f792ff1
Luis Felipe Fezer <luisfelipefezer@gmail.com>
2022-04-26 22:54:38 -0300

    Added routers to clientRouter.js file

--- src/routes/clientRouter.js
+++ src/routes/clientRouter.js
@@ -1,4 +1,12 @@
 const express = require("express");
 const clientRouter = express.Router();
+const clientController = require("../controllers/clientController");
+const auth = require("../middlewares/auth");
+
+clientRouter.post("/createClient", auth, clientController.createClient);
+clientRouter.get("/listAllClients", auth, clientController.listAllClients);
+clientRouter.get("/listClient", auth, clientController.listClient);
+clientRouter.put("/editClient", auth, clientController.editClient);
+clientRouter.delete("/removeClient", auth, clientController.removeClient);
 
 module.exports = clientRouter;
