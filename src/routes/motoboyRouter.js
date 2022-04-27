d11d4a2ec93210f77f88253ab2788c8315385f86
Luis Felipe Fezer <luisfelipefezer@gmail.com>
2022-04-26 22:58:12 -0300

    Added routers to motoboyRouter.js file

--- src/routes/motoboyRouter.js
+++ src/routes/motoboyRouter.js
@@ -1,4 +1,12 @@
 const express = require("express");
 const motoboyRouter = express.Router();
+const motoboyController = require("../controllers/clientController");
+const auth = require("../middlewares/auth");
+
+motoboyRouter.post("/createMotoboy", auth, motoboyController.createMotoboy);
+motoboyRouter.get("/listAllMotoboys", auth, motoboyController.listAllMotoboys);
+motoboyRouter.get("/listMotoboy", auth, motoboyController.listMotoboy);
+motoboyRouter.put("/editMotoboy", auth, motoboyController.editMotoboy);
+motoboyRouter.delete("/removeMotoboy", auth, motoboyController.removeMotoboy);
 
 module.exports = motoboyRouter;
