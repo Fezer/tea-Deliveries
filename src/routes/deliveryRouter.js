97811387b8ad434719497b9de519b519c67de184
Luis Felipe Fezer <luisfelipefezer@gmail.com>
2022-04-26 22:56:56 -0300

    Added routers to deliveryRouter.js file

--- src/routes/deliveryRouter.js
+++ src/routes/deliveryRouter.js
@@ -1,4 +1,14 @@
 const express = require("express");
 const deliveryRouter = express.Router();
+const deliveryController = require("../controllers/deliveryController");
+const auth = require("../middlewares/auth");
+
+deliveryRouter.post("/createDelivery", auth, deliveryController.createDelivery);
+deliveryRouter.get("/listAllDeliveries", auth, deliveryController.listAllDeliveries);
+deliveryRouter.get("/listAllDoneDeliveries", auth, deliveryController.listAllDoneDeliveries);
+deliveryRouter.get("/listAllPendingDeliveries", auth, deliveryController.listAllPendingDeliveries);
+deliveryRouter.get("/listAllDeliveriesByMotoboy", auth, deliveryController.listAllDeliveriesByMotoboy);
+deliveryRouter.put("/editPendingDelivery", auth, deliveryController.editPendingDelivery);
+deliveryRouter.delete("/removePendingDelivery", auth, deliveryController.removePendingDelivery);
 
 module.exports = deliveryRouter;
