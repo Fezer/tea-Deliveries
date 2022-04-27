25e4b9ded82dd9d951f0893ed04bddfbbfbf7750
Luis Felipe Fezer <luisfelipefezer@gmail.com>
2022-04-26 22:32:41 -0300

    Added basic method strucure to deliveryCotroller

---
+++ src/controllers/deliveryController.js
@@ -0,0 +1,37 @@
+const Associate = require("../models/Associate");
+const Client = require("../models/Client");
+const Delivery = require("../models/Delivery");
+const Motoboy = require("../models/Motoboy");
+const sequelize = require("sequelize");
+
+module.exports = {
+
+    async createDelivery(req, res) {
+        
+    },
+    
+    async listAllDeliveries(req, res) {
+        
+    },
+
+    async listAllDoneDeliveries(req, res) {
+        
+    },
+
+    async listAllPendingDeliveries(req, res) {
+        
+    },
+
+    async listAllDeliveriesByMotoboy(req, res) {
+        
+    },
+
+    async editPendingDelivery(req, res) {
+        
+    },
+
+    async removePendingDelivery(req, res) {
+        
+    },
+
+};
