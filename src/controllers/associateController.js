caf8e8251d676c18eb0d709aca09c5c6660fefe8
Luis Felipe Fezer <luisfelipefezer@gmail.com>
2022-04-26 22:23:33 -0300

    Added associateController.js file with basic scruture

---
+++ src/controllers/associateController.js
@@ -0,0 +1,11 @@
+const Associate = require("../models/Associate");
+const Client = require("../models/Client");
+const Delivery = require("../models/Delivery");
+const Motoboy = require("../models/Motoboy");
+const sequelize = require("sequelize");
+
+module.exports = {
+
+
+
+};
