8a3aeaeca8579fce272674907100f8d387049fe0
Luis Felipe Fezer <luisfelipefezer@gmail.com>
2022-05-03 19:25:22 -0300

    Basic adjustments at deliveries migration. Seeders creation

---
+++ src/database/seeders/20220503220113-clients.js
@@ -0,0 +1,29 @@
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    return queryInterface.bulkInsert(
+      "Clients",
+      [
+        {
+          name: "Cliente 1",
+          cnpj: "111",
+          address: "Rua do cliente 1",
+        },
+        {
+          name: "Cliente 2",
+          cnpj: "222",
+          address: "Rua do cliente 2",
+        },
+        {
+          name: "Cliente 3",
+          cnpj: "333",
+          address: "Rua do cliente 3",
+        },
+      ],
+      {}
+    );
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    return queryInterface.bulkDelete("Clients", null, {});
+  },
+};
